60473886802d496f1c9adabb2aad061ee0f1db95
darbestor <fordarbe@gmail.com>
2023-12-02 20:44:44 +0300

    day 1 part 2 complete

--- src/days/day1/mod.rs
+++ src/days/day1/mod.rs
@@ -7,7 +7,7 @@ pub fn solve() -> Answer {
     Answer {
         day: 1,
         part1: part1(input),
-        part2: ResultType::None,
+        part2: part2(input),
     }
 }
 
@@ -22,4 +22,52 @@ fn part1(input: &str) -> ResultType {
             })
             .sum(),
     )
+}
+
+static NUMBERS: [&[u8]; 9] = [
+    "one".as_bytes(),
+    "two".as_bytes(),
+    "three".as_bytes(),
+    "four".as_bytes(),
+    "five".as_bytes(),
+    "six".as_bytes(),
+    "seven".as_bytes(),
+    "eight".as_bytes(),
+    "nine".as_bytes(),
+];
+
+fn part2(input: &str) -> ResultType {
+    ResultType::Usize(
+        input
+            .split('\n')
+            .map(|line| {
+                let numbers = find_numbers(line);
+                numbers.last().unwrap() + (numbers.first().unwrap() * 10) as usize
+            })
+            .sum(),
+    )
+}
+
+fn find_numbers(str: &str) -> Vec<usize> {
+    let mut numbers = Vec::new();
+    let str = str.as_bytes();
+    for i in 0..str.len() {
+        if str[i].is_ascii_digit() {
+            numbers.push((str[i] - ASCII_ZERO) as usize);
+        }
+        for (number, &num_str) in NUMBERS.iter().enumerate() {
+            let mut j = 0;
+            for &ch in num_str {
+                let next = i + j;
+                if next >= str.len() || ch != str[next] {
+                    break;
+                }
+                j += 1;
+            }
+            if j == num_str.len() {
+                numbers.push(number + 1);
+            }
+        }
+    }
+    numbers
 }
